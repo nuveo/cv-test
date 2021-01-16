282b859ebb35e93fa7d1478c87cf88667b47bfcf
Matheus Alan Bergmann <matheus.bergmann@pixforce.com.br>
2021-01-16 19:13:45 -0300

    :sparkles: Correct skew

--- 01-DocumentCleanup/cleaner.py
+++ 01-DocumentCleanup/cleaner.py
@@ -1,6 +1,6 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+from scipy.ndimage import interpolation as inter
 
 
 def clean_document(document):
@@ -9,7 +9,47 @@ def clean_document(document):
     :param document: BGR image with the document
     :return: binary image with background in 255 and text in 0
     """
-    new_image = cv2.cvtColor(document, cv2.COLOR_BGR2GRAY)
-    new_image = cv2.adaptiveThreshold(new_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 30)
-
+    new_image = binarize_image(document)
+    new_image = correct_skew(new_image)
     return new_image
+
+
+def binarize_image(image):
+    new_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    new_image = cv2.adaptiveThreshold(new_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 30)
+    return new_image
+
+
+def correct_skew(binary_image):
+    """
+    Rotates the image in every angle between [-45, 45] (with step 1)
+    and returns the version with best skew score.
+    Further reading: https://towardsdatascience.com/pre-processing-in-ocr-fc231c6035a7
+    :param binary_image: Image with bg in 255 and text in 0
+    :return: An deskewed image with bg in 255 and text in 0
+    """
+    best_image = None
+    best_value = -1
+    binary_image = 255 - binary_image
+    for angle in range(-45, 45):  # The range is -45 to 45 to avoid finding an upside-down image
+        rotated = inter.rotate(binary_image, angle, reshape=False, order=0)
+        score = calculate_skew_score(rotated)
+        if score > best_value:
+            best_image = rotated
+            best_value = score
+
+    best_image = 255 - best_image
+
+    return best_image
+
+
+def calculate_skew_score(binary_image):
+    """
+    Calculates the skew score to a certain image. The higher the better.
+    Further reading: https://towardsdatascience.com/pre-processing-in-ocr-fc231c6035a7
+    :param binary_image: Image with bg in 255 and text in 0
+    :return: An int with the score saying how aligned is binary_image
+    """
+    hist = np.sum(binary_image, axis=1)
+    score = np.sum((hist[1:] - hist[:-1]) ** 2)
+    return score
--- 01-DocumentCleanup/tests.py
+++ 01-DocumentCleanup/tests.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from scipy.ndimage import interpolation as inter
 import unittest
 
 import cleaner
@@ -16,3 +18,34 @@ class TestCleanerMethods(unittest.TestCase):
         result = cleaner.clean_document(test_image)
         valid_values = (result == 0) | (result == 255)
         self.assertTrue(valid_values.all())
+
+    def test_correct_skew_shape(self):
+        test_image = cv2.imread("noisy_data/2.png")
+        binary_image = cleaner.binarize_image(test_image)
+        result = cleaner.correct_skew(binary_image)
+        self.assertEqual(result.shape, test_image.shape[:2])
+
+    def test_correct_skew_is_binary(self):
+        test_image = cv2.imread("noisy_data/2.png")
+        binary_image = cleaner.binarize_image(test_image)
+        result = cleaner.correct_skew(binary_image)
+        valid_values = (result == 0) | (result == 255)
+        self.assertTrue(valid_values.all())
+
+    def test_skew_score(self):
+        test_image_aligned = np.zeros((100, 100), dtype=np.uint8)
+        test_image_aligned[0:10, :] = 255
+        test_image_aligned[20:30, :] = 255
+        test_image_aligned[40:50, :] = 255
+        test_image_aligned[60:70, :] = 255
+        test_image_aligned[80:90, :] = 255
+
+        rotated_minus_10 = inter.rotate(test_image_aligned, -10, reshape=False, order=0)
+        rotated_10 = inter.rotate(test_image_aligned, -10, reshape=False, order=0)
+
+        score_aligned = cleaner.calculate_skew_score(test_image_aligned)
+        score_minus_10 = cleaner.calculate_skew_score(rotated_minus_10)
+        score_10 = cleaner.calculate_skew_score(rotated_10)
+
+        self.assertGreater(score_aligned, score_minus_10)
+        self.assertGreater(score_aligned, score_10)
