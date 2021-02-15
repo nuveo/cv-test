040dff2c5e98f1a3fb22fc87980af56fdc3e9219
NataliaCarvalho03 <carvalho.natalia03@gmail.com>
2021-02-15 14:27:35 -0300

    SpamDetection

---
+++ SpamDetection/README.md
@@ -0,0 +1,27 @@
+# Spam Detection
+
+## Rodar o projeto
+
+Lembre-se de instalar as dependências:
+
+```
+pip install -r requirements.txt
+```
+
+Para rodar o projeto você deve utilizar o seguinte comando:
+
+```
+python main.py caminho-para-dataset-de-treino caminho-para-dataset-de-teste
+```
+
+## Técnicas utilizadas
+
+Para solucionar o problema de detecção de mensagens de spam, utilizei o classificador Nayve Bayes.
+
+Utilizei algumas referencias que podem ser conferidas em:
+
+https://www.kaggle.com/amitkarmakar41/spamclassifier
+
+https://www.kaggle.com/pramodsivakumar/spam-sms-classifier-98-3-accuracy
+
+https://www.kdnuggets.com/2020/07/spam-filter-python-naive-bayes-scratch.html
---
+++ SpamDetection/main.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import re, nltk, sys
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+def tokenize(dataFrame, dataset_type='train'):
+    ps = PorterStemmer()
+
+    if dataset_type == 'train':
+
+        corpus = []
+        for i in range(0, len(dataFrame)):
+            review = re.sub('[^a-zA-Z]', ' ', dataFrame[1][i])
+            review = review.lower()
+            review = nltk.word_tokenize(review)
+            
+            review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+            review = ' '.join(review)
+            corpus.append(review)
+    
+    elif dataset_type == 'test':
+
+        corpus = []
+        for i in range(0, len(dataFrame)):
+            review = re.sub('[^a-zA-Z]', ' ', dataFrame[0][i])
+            review = review.lower()
+            review = nltk.word_tokenize(review)
+            
+            review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+            review = ' '.join(review)
+            corpus.append(review)
+    
+    return corpus
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        print("\nPlease, pass the path to train and test dataset! Aborting...\n")
+        sys.exit()
+    
+    train = pd.read_csv(sys.argv[1], sep='\t', header=None)
+    #print(train.head())
+
+    test = pd.read_csv(sys.argv[2], sep='\t', header=None)
+    #print(test.head())
+
+    tokenized_train = tokenize(train)
+    tokenized_test = tokenize(test, 'test')
+
+    cv = CountVectorizer(max_features = 2000)
+    X_train = cv.fit_transform(tokenized_train).toarray()
+    X_test = cv.fit_transform(tokenized_test).toarray()
+    
+    Y_train = pd.get_dummies(train[0])
+    Y_train = Y_train.iloc[:,1].values
+
+    print(train[0].head(),"\n y = ",Y_train) # ham = 0 and spam = 1
+
+    spam_detect_model = MultinomialNB()
+    spam_detect_model.fit(X_train, Y_train)
+
+    predictions = spam_detect_model.predict(X_test)
+
+    labels = []
+
+    for predicted in predictions:
+        if predicted:
+            labels.append('spam')
+        else:
+            labels.append('ham')
+
+    test.insert(0, "",labels, True)
+
+    test.to_csv(sys.argv[2], sep='\t', header=None, index=False)
